f91b90714d2a1c3dc4712ad9710b7056815bb73b
sssmart24 <ssmart24@aoiths.org>
2023-10-06 12:41:41 -0400

    2.3 oct6

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using System.Collections.Generic; 
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +18,13 @@ public class SpawnManager : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
+        // Randomly generate animal index and spawn position 
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
+        0, spawnPosZ);
+
         {
-            Instantiate(animalPrefabs[animalIndex],
+            int animalIndex = Random.Range(0, animalPrefabs.Length); 
+            Instantiate(animalPrefabs[animalIndex], spawnPos,
             new Vector3 (0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
         }
     }
