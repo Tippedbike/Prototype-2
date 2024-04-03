863217d458ac0bee450d38f59ee2907603cacb33
TippedBike89 <jgalindo-miranda25@aoiths.org>
2024-04-03 16:45:52 -0400

    poop

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class DetectCollisions : MonoBehaviour
 {
-    
+    private int score; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0;
+        UpdateScore(0);
     }
     void OnTriggerEnter(Collider other)
     {
+        if (gameObject )
+        {
         Destroy(gameObject);
         Destroy(other.gameObject);
+        }
     }
     // Update is called once per frame
-    void Update()
+   public void UpdateScore(int scoreToAdd)
     {
-        
+        score += scoreToAdd;
+        Debug.Log("Score: " + score);
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int score; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        Debug.Log("Score: " + score);
+    }
+}
